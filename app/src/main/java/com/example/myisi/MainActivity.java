330b0719a464d3337fc6b5b57d98fcb6f5b87034
niemet0502 <vincentmarius8@gmail.com>
2021-01-05 22:01:56 +0100

    main :: control btn connect action

--- app/src/main/java/com/example/myisi/MainActivity.java
+++ app/src/main/java/com/example/myisi/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.myisi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnConnect, btnSignUp;
+    private EditText txtLogin, txtPassword;
+    private String  login, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnConnect = findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);        
+            }
+        });
     }
 }
