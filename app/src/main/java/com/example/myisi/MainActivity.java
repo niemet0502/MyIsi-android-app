a0db36e26e67e4e1399a457defa36dab37f8dbb7
niemet0502 <vincentmarius8@gmail.com>
2021-01-19 21:29:42 +0100

    main :: added Main Activity

--- app/src/main/java/com/example/myisi/MainActivity.java
+++ app/src/main/java/com/example/myisi/MainActivity.java
@@ -9,21 +9,29 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnConnect, btnSignUp;
+
     private EditText txtLogin, txtPassword;
-    private String  login, password;
+    private Button btnConnect, btnSignUp;
+    private String login, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //link entre les variables et les composants sur GUI
         setContentView(R.layout.activity_main);
+        txtLogin = findViewById(R.id.txtLogin);
+        txtPassword = findViewById(R.id.txtPassword);
         btnConnect = findViewById(R.id.btnConnect);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        //Gestion events
+
         btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);        
+                startActivity(intent);//Acceder à la page HomeActivity
             }
         });
+
     }
 }
